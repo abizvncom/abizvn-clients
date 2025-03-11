666c1e32c1b3ce795352e8db5fd0001f4ce265d7
huan <huanhvhd@gmail.com>
2025-03-11 18:20:29 +0700

    Make admin user email optional

--- app/models/abizvn/clients/client.rb
+++ app/models/abizvn/clients/client.rb
@@ -8,7 +8,7 @@ module Abizvn
       has_many :domains, class_name: 'Abizvn::Clients::Domain', foreign_key: :client_id
       accepts_nested_attributes_for :domains, allow_destroy: true
 
-      validates :admin_user_email, presence: true, uniqueness: { case_sensitive: false }, 'valid_email_2/email': true
+      validates :admin_user_email, presence: true, 'valid_email_2/email': true
 
       validates :name, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 128 }
       validates :contact_email, 'valid_email_2/email': true
